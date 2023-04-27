1617a2ead3456eefce5970022a78b175c7803a9a
Mariquena <allosamariquena@gmail.com>
2023-04-27 16:42:22 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CommonComponent } from './common/common.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CommonComponent
   ],
   imports: [
     BrowserModule,
