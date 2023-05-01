fc0347acac6226238a5f3d454a1380a6b0a45063
Pautk <pautk7.6@gmail.com>
2023-05-01 18:11:37 -0300

    Agregado de componentes carrucel de destacadas y about

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CarrucelDestComponent } from './carrucel-dest/carrucel-dest.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CarrucelDestComponent,
+    AboutComponent
+  ],
   imports: [
     CommonModule
   ]
