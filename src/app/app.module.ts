272bb63b946a22456f75dd43298af30cd2cd4b27
Mariquena <allosamariquena@gmail.com>
2023-05-03 16:44:17 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-
-// Módulos
 import { AppRoutingModule } from './app-routing.module';
 
 
