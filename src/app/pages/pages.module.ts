e2c299f799543a8057f66cb740f58365438a3e9b
Pautk <pautk7.6@gmail.com>
2023-05-05 20:52:33 -0300

    agrego componente login y contenido

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule
