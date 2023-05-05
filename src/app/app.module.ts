a87e745096a2b513700a1226d2218e63ee2c368c
Mariquena <allosamariquena@gmail.com>
2023-05-05 07:52:26 -0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { PagesModule } from './pages/pages.module';
 
-
-// Componentes 
-import { AdminComponent } from './pages/admin/admin.component';
-import { AboutComponent } from './shared/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminComponent,
-    AboutComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    CommonModule,
+    PagesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
