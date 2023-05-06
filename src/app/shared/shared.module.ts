7ab353e9def6a3f90b40ac2485d50ac7c1118517
Pautk <pautk7.6@gmail.com>
2023-05-06 12:03:23 -0300

    agrego c banner más contenido

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CarrucelDestComponent } from './carrucel-dest/carrucel-dest.component';
 import { AboutComponent } from './about/about.component';
+import { BannerCarouselComponent } from './banner-carousel/banner-carousel.component';
 
 
 
 @NgModule({
   declarations: [
     CarrucelDestComponent,
-    AboutComponent
+    AboutComponent,
+    BannerCarouselComponent
   ],
   imports: [
     CommonModule
