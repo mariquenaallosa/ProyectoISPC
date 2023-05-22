7a4b0ad0e89386ff5ec87d088da9b0a082a32e95
Mariquena <allosamariquena@gmail.com>
2023-05-21 22:33:14 -0300

    Agregar propiedades en routing

--- View/src/app/app-routing.module.ts
+++ View/src/app/app-routing.module.ts
@@ -6,14 +6,19 @@ import { LoginComponent } from './pages/login/login.component';
 import { UsersComponent } from './pages/users/users.component';
 import { Pagina404Component } from './pages/pagina404/pagina404.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { AboutComponent } from './shared/about/about.component';
+import { GaleriaComponent } from './pages/galeria/galeria.component';
 
 //Rutas de navegacion
 const routes: Routes = [
-  {path: 'home', component: HomeComponent},
+  {path: 'home', component: HomeComponent, children:[
+    {path: 'home-nosotros', component: AboutComponent}
+  ]},
   {path: 'login', component: LoginComponent},
   {path: 'admin', component: AdminComponent},
   {path: 'users', component: UsersComponent},
   {path: 'registro', component: RegistroComponent},
+  {path: 'propiedades',  component:GaleriaComponent},
   //{path: '**', component: Pagina404Component},
   {path: '', redirectTo: '/home', pathMatch: 'full' }, // Ruta por defecto
 ];
