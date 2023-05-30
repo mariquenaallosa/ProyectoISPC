d1b91af182e41ea7f71e58f9af1b1333304ddfb7
alfred <fornarychiky@gmail.com>
2023-05-29 22:28:21 -0400

    validacion Ingresar

--- View/src/app/pages/login/login.component.ts
+++ View/src/app/pages/login/login.component.ts
@@ -7,17 +7,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  
-  get email(){
-    return this.formLogin.get('email') as FormControl;
-  }
-  get password(){
-    return this.formLogin.get('password') as FormControl;
-  }
-  
-  formLogin= new FormGroup({
-    'email': new FormControl ('', [Validators.required, Validators.email]),
-    'password': new FormControl('',Validators.required),
-  })
+
+
+  email=new FormControl('',[Validators.required,Validators.email]);
+  password=new FormControl('',Validators.required);
 
 }
