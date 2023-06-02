619b831f7cfdde5815bd7cc8780542dfcc3e6848
Pautk <pautk7.6@gmail.com>
2023-06-02 13:33:01 -0300

    ruteo registro

--- View/src/app/pages/registro/registro.component.ts
+++ View/src/app/pages/registro/registro.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, AbstractControl, FormControl } from '@angular/forms';
+import { passwordMatch } from './validatorsPass';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
@@ -7,75 +9,34 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
-  form!:FormGroup;
+  formRegistro = new FormGroup({
+    nombre : new FormControl("", [Validators.required, Validators.maxLength(32)]),
+    apellido : new FormControl("", [Validators.required, Validators.maxLength(32)]),
+    email : new FormControl("", [Validators.required, Validators.email]),
+    password : new FormControl("", [Validators.required, Validators.minLength(8)]),
+    confirm_password : new FormControl("", [Validators.required, Validators.minLength(8)])
 
-  constructor(private fb:FormBuilder){
-    this.registro();
-  }
+  }, [ passwordMatch("password", "confirm_password") ])
 
-  get nombreNovalido(){
-    return this.form.get('nombre')?.invalid && this.form.get('nombre')?.touched;
-  }
-  get apellidoNovalido(){
-    return this.form.get('apellido')?.invalid && this.form.get('apellido')?.touched;
-  }
-  get emailNovalido(){
-    return this.form.get('email')?.invalid && this.form.get('email')?.touched
-  }
-  get password1Novalido(){
-    return this.form.get('password1')?.invalid && this.form.get('password1')?.touched;
-  }
-  get password2Novalido(){
-    return this.form.get('password2')?.invalid && this.form.get('password2')?.touched;
+  getControl(name: any): AbstractControl | null {
+    return this.formRegistro.get(name)
   }
 
-  registro(){
-    this.form= this.fb.group({
-      nombre:['', Validators.required],
-      apellido:['', Validators.required],
-      email:['', [Validators.required, Validators.email]],
-      password1: ['', [Validators.required,Validators.minLength(8)]],
-      password2: ['', Validators.required],
+  constructor(private router: Router){
 
-    },{
-    Validators:this.passwordIguales('password1', 'password2'),
-    })
   }
-
-  btnRegistro(){
-    console.log(this.form);
-    this.passNovalido();
-
-    if (this.form.invalid){
-      return Object.values(this.form.controls).forEach(control=>{
-        control.markAllAsTouched();
-      })
+  registerFn(){
+    if(this.formRegistro.valid){
+      this.router.navigateByUrl('/users'),
+      this.formRegistro.reset()
+    }
+    else{
+      this.formRegistro.markAllAsTouched()
+      alert('Error al ingresar los datos.')
     }
   }
-
-  passNovalido(){
-    const pass1= this.form.get('password1')?.value;
-    const pass2= this.form.get('password2')?.value;
-
-    if (pass1 !== pass2){
-      return true;
-    }else{
-      return false;
-    }
-  }
-
-  passwordIguales(pass1Name:string, pass2Name:string){
-    return (formGroup:FormGroup) =>{
-      const pass1Control = formGroup.get(pass1Name);
-      const pass2Control = formGroup.get(pass2Name);
-
-      if(pass1Control?.value === pass2Control?.value){
-        pass2Control?.setErrors(null);
-      }else{
-        pass2Control?.setErrors({noEsIgual:false})
-      }
-        
-      
-    }
-  }
+  
+  
+  
+  
 }
